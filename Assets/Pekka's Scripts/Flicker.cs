f0f15d8e66c24c7ecefd5df413e9228935fff5e3
Pekka Kääriäinen <l5478@student.jamk.fi>
2019-10-10 15:36:27 +0200

    light

---
+++ Assets/Pekka's Scripts/Flicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    public float sec;
+    public Light light;
+
+    private void Start()
+    {
+        StartCoroutine("LightOn", sec);
+    }
+
+    IEnumerator LightOn(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            if (light.enabled)
+            {
+                light.enabled = false;
+                GetComponent<SphereCollider>().enabled = true;
+            }
+            else
+            {
+                light.enabled = true;
+                GetComponent<SphereCollider>().enabled = false;
+            }
+        }
+    }
+
+
+}
