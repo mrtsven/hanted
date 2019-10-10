83b279e4e2e02c3b0aade57839ed07dc0f17fa33
mrtsven <zahharov@hotmail.com>
2019-10-10 15:33:23 +0200

    trying to fix fieldofview radius

---
+++ Assets/Scripts/EnemyScripts/FieldOfView.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class FieldOfView : MonoBehaviour
+{
+
+	public float viewRadius;
+	[Range(0,360)]
+	public float viewAngle;
+
+    public float stopBeforeTarget = 6;
+
+	public LayerMask targetMask;
+	public LayerMask obstacleMask;
+
+	[HideInInspector]
+	public List<Transform> visibleTargets = new List<Transform>();
+
+    private NavMeshAgent agent;
+
+	void Start() {
+		StartCoroutine ("FindTargetsWithDelay", .2f);
+
+        agent = GetComponent<NavMeshAgent>();
+	}
+
+
+	IEnumerator FindTargetsWithDelay(float delay) {
+		while (true) {
+			yield return new WaitForSeconds (delay);
+			FindVisibleTargets ();
+		}
+	}
+
+	void FindVisibleTargets() {
+		visibleTargets.Clear ();
+		Collider[] targetsInViewRadius = Physics.OverlapSphere (transform.position, viewRadius, targetMask);
+
+		for (int i = 0; i < targetsInViewRadius.Length; i++) {
+			Transform target = targetsInViewRadius [i].transform;
+			Vector3 dirToTarget = (target.position - transform.position).normalized;
+			if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
+				float dstToTarget = Vector3.Distance (transform.position, target.position);
+
+				if (!Physics.Raycast (transform.position, dirToTarget, dstToTarget, obstacleMask)) {
+					// Found target
+                    visibleTargets.Add (target);
+                    agent.stoppingDistance = stopBeforeTarget;
+                    agent.SetDestination(target.position);
+                    if(Vector3.Distance(agent.transform.position, target.position) < 2){
+                        // SceneManager.LoadScene("");
+                    }
+				}
+			}
+		}
+	}
+
+
+	public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+		if (!angleIsGlobal) {
+			angleInDegrees += transform.eulerAngles.y;
+		}
+		return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+	}
+}
